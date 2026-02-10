007b81fa2f4bbfdfe9a3926322825bef240e57be
ishern08 <144407567+ishern08@users.noreply.github.com>
2026-02-10 10:38:55 -0600

    changed the main code

--- Program.cs
+++ Program.cs
@@ -6,32 +6,64 @@ class Program
 {
     static void Main(string[] args)
     {
-        int? x1;
-        int? x2;
+        //did this for a commit
+        // simulated change from teammate on github
+        //adding a new comment
+        Account acc1 = new Account("Susan", 100);
+        Account acc2 = new Account("Bonnie", 100);
+        acc1.Show();
+        acc1.Withdraw(acc1.GetBalance(), 150);
+        acc1.Show();
+        acc1.Deposit(acc1.GetBalance(), 200);
+        acc1.Show();
+        acc1.Withdraw(acc1.GetBalance(), 150);
+        acc1.Show();
+        acc2.Show();
+        acc1.Transfer(acc2, acc1.GetBalance(), 50);
+        acc1.Show();
+        acc2.Show();
 
-        x1 = 20;
-        x2 = null;
+        //testing that balance getter is working(100)
+        Console.WriteLine(acc1.Balance);
 
-        pracMethod(x1);
-        pracMethod(x2);
+        //testing that the balance setter is working(500)
+        acc1.Balance = 500;
+        Console.WriteLine(acc1.Balance);
+
+        //testing that it doesn't work for negative values(500)
+        acc1.Balance = -100;
+        Console.WriteLine(acc1.Balance);
+
+        //testing that owner getter is working(susan)
+        Console.WriteLine(acc1.owner);
+
+        //testing that owner setter doesn't work which it doesn't because it creates a syntax error
+        //acc1.owner = "Alice";*/
 
 
+        Account a1 = new Account("bob", 100);
+        Account a2 = new Account("fred", 200);
 
 
+        a1 = a2;
+        a1.Show();
+        a2.Show();
+        a1.Withdraw(a1.Balance, 10);
+        a2.Withdraw(a2.Balance, 20);
+        a1.Show();
+        a2.Show();
+        int x = 5, y = 10;
+        x = y;
+        x++;
+        Console.WriteLine(x);
+        Console.WriteLine(y);
 
-
-    }
-
-    public static void pracMethod(int? val)
-    {
-        if (val == null)
-        {
-            Console.WriteLine("The parameter does not have a value.");
-        }
-        else
-        {
-            Console.WriteLine(val);
-        }
+        // in main
+        int totalAssets = 1000;
+        doIt(a1, totalAssets);
+        Console.WriteLine("after doIt:");
+        a1.Show();
+        Console.WriteLine($"total assets is now {totalAssets}");
     }
 }
 
